855ad3a9eace8452dee4a9783114401a56f5e524
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2026-06-23 08:31:35 -0300

    Add Logs Quantidades

---
+++ src/coletarRaposo.ts
@@ -0,0 +1,150 @@
+import "dotenv/config"
+import axios from "axios"
+import { startOfWeek, endOfWeek, subWeeks, addWeeks, format } from 'date-fns'
+
+import DbOracle from "./database/connectionManager.js"
+
+const syncCarrosGlobus = async () => {
+  const db = DbOracle.getConnection()
+
+  const data = await db.raw(`
+    select
+      codigoveic,
+      CODIGOTPFROTA,
+      placaatualveic,
+      prefixoveic,
+      condicaoveic,
+      CODIGOEMPRESA
+    from
+      FRT_CADVEICULOS
+    order by
+      PREFIXOVEIC
+  `)
+
+  console.log("Enviando dados de carros para a API do Globus...")
+  console.log(`Total de carros a serem enviados: ${data.length}`)
+
+  await axios.post("https://login.teleconsult.com.br/api/inbound/globus/carros", {
+    id_empresa: process.env.ID_EMPRESA,
+    token: process.env.TOKEN,
+    data: data
+  })
+}
+
+const syncLinhasGlobus = async () => {
+  const db = DbOracle.getConnection()
+
+  const data = await db.raw(`
+    select
+      CODINTLINHA,
+      CODIGOORGCONC,
+      CODIGOLINHA,
+      NROFICIALLINHA,
+      NOMELINHA,
+      CODIGOEMPRESA
+    from
+      bgm_cadlinhas
+    where
+      CODIGOORGCONC is not null
+  `)
+
+  console.log("Enviando dados de linhas para a API do Globus...")
+  console.log(`Total de linhas a serem enviadas: ${data.length}`)
+
+  await axios.post("https://login.teleconsult.com.br/api/inbound/globus/linhas", {
+    id_empresa: process.env.ID_EMPRESA,
+    token: process.env.TOKEN,
+    data: data
+  })
+}
+
+const syncMotoristasGlobus = async () => {
+  const db = DbOracle.getConnection()
+
+  const data = await db.raw(`
+    select
+      f.codintfunc,
+      f.chapafunc,
+      f.nomefunc,
+      f.codfunc,
+      f.apelidofunc,
+      f.codigoempresa
+    from
+      flp_funcionarios f
+    where
+      f.SITUACAOFUNC = 'A'  
+  `)
+
+  console.log("Enviando dados de motoristas para a API do Globus...")
+  console.log(`Total de motoristas a serem enviados: ${data.length}`)
+
+  await axios.post("https://login.teleconsult.com.br/api/inbound/globus/motoristas", {
+    id_empresa: process.env.ID_EMPRESA,
+    token: process.env.TOKEN,
+    data: data
+  })
+}
+
+const syncViagensGlobus = async () => {
+  const inicio = startOfWeek(subWeeks(new Date(), 1), { weekStartsOn: 1 })
+  const fim = endOfWeek(addWeeks(new Date(), 0), { weekStartsOn: 1 })
+
+  console.log(`Sincronizando dados de ${format(inicio, 'dd/MM/yyyy HH:mm:ss')} a ${format(fim, 'dd/MM/yyyy HH:mm:ss')}`)
+
+  const db = DbOracle.getConnection()
+
+  const data = await db.raw(`
+      select
+        to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd' ) dt,
+        prefixoveic,
+        l.codigolinha,
+        l.nomelinha,
+        l.codintlinha,
+        codigoorgconc,
+        '' cod_servdiaria,
+        to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dti,
+        to_char( t_arr_viagens_guia.QTD_HORA_FINAL , 'yyyy-mm-dd hh24:mi:ss' ) dtf,
+        frt_cadveiculos.CODIGOTPFROTA cdft,
+        fm.codfunc f1cod,
+        fm.apelidofunc f1ap,
+        fm.nomefunc f1nome,
+        to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dtg,
+        frt_cadveiculos.CODIGOEMPRESA,
+        fm.CODINTFUNC as CODIGO_FUNCIONARIO
+      from
+        t_arr_viagens_guia
+        left join frt_cadveiculos
+          on frt_cadveiculos.codigoveic = t_arr_viagens_guia.COD_VEICULO
+        Join bgm_cadlinhas l
+          on t_arr_viagens_guia.cod_intlinha = l.codintlinha
+        left join T_ARR_TROCAS_FUNC
+          on T_ARR_TROCAS_FUNC.COD_SEQ_GUIA = t_arr_viagens_guia.COD_SEQ_GUIA and FLG_MOT_COB = 'M'
+        left Join flp_funcionarios fm
+          on fm.CODINTFUNC = T_ARR_TROCAS_FUNC.CODINTFUNC
+      where
+        -- frt_cadveiculos.CODIGOEMPRESA = id_empresa and
+        t_arr_viagens_guia.QTD_HORA_INI between to_date('${format(inicio, 'yyyy-MM-dd HH:mm:ss')}','yyyy-mm-dd hh24:mi:ss') and to_date('${format(fim, 'yyyy-MM-dd HH:mm:ss')}','yyyy-mm-dd hh24:mi:ss') 
+  `)
+
+  console.log("Enviando dados de viagens para a API do Globus...")
+  console.log(`Total de viagens a serem enviadas: ${data.length}`)
+
+  await axios.post("https://login.teleconsult.com.br/api/inbound/globus/viagens", {
+    id_empresa: process.env.ID_EMPRESA,
+    token: process.env.TOKEN,
+    data: data
+  })
+}
+
+const executar = async () => {
+  try {
+    await syncCarrosGlobus()
+    await syncLinhasGlobus()
+    await syncMotoristasGlobus()
+    await syncViagensGlobus()
+  } catch (error) {
+    console.error("Erro ao sincronizar dados com a API do Globus:", error)
+  }
+}
+
+executar()
