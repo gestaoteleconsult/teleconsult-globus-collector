3bfc2ef51a554bbf5fb573d58451a2798ec1fb62
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2026-07-21 22:49:02 -0300

    Update

--- src/coletarRaposo.ts
+++ src/coletarRaposo.ts
@@ -87,10 +87,10 @@ const syncMotoristasGlobus = async () => {
 }
 
 const syncViagensGlobus = async () => {
-  // const inicio = startOfWeek(subWeeks(new Date(), 1), { weekStartsOn: 1 })
-  // const fim = endOfWeek(addWeeks(new Date(), 0), { weekStartsOn: 1 })
-  const inicio = parseISO('2026-05-01T00:00:00')
-  const fim = parseISO('2026-07-22T23:59:59')
+  const inicio = startOfWeek(subWeeks(new Date(), 1), { weekStartsOn: 1 })
+  const fim = endOfWeek(addWeeks(new Date(), 0), { weekStartsOn: 1 })
+  // const inicio = parseISO('2026-05-01T00:00:00')
+  // const fim = parseISO('2026-07-22T23:59:59')
   // const fim = parseISO('2024-06-24T23:59:59')
 
   console.log(`Sincronizando dados de ${format(inicio, 'dd/MM/yyyy HH:mm:ss')} a ${format(fim, 'dd/MM/yyyy HH:mm:ss')}`)
